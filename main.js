d69189f0febfdad2bf3f2a9a76c7348503ecf176
Danielle <danielle@kendallspace.net>
2024-05-03 19:25:22 -0700

    reorganize code

--- main.js
+++ main.js
@@ -1,21 +1,10 @@
-const carousels = document.querySelectorAll('[data-carousel]');
-
-carousels.forEach(setUpCarousel);
+function modulo(number, mod) {
+  let result = number % mod;
+  if (result < 0) result += mod;
+  return result;
+}
 
 function setUpCarousel(carousel) {
-  const buttonPrevious = carousel.querySelector(
-    '[data-carousel-button-previous]'
-  );
-  const buttonNext = carousel.querySelector(
-    '[data-carousel-button-next]'
-  );
-  const slidesContainer = carousel.querySelector(
-    '[data-carousel-slides-container]'
-  );
-
-  let currentSlide = 0;
-  const numberOfSlides = slidesContainer.childElementCount;
-
   function handleNextSlide() {
     currentSlide = modulo(currentSlide + 1, numberOfSlides);
     changeSlide(currentSlide);
@@ -27,30 +16,39 @@ function setUpCarousel(carousel) {
   }
 
   function changeSlide(slideNumber) {
-    // change current slide visually
+    // change current slide in the DOM
     carousel.style.setProperty('--current-slide', slideNumber);
 
     // handle screen reader accessibility
-    // here we're getting the elements for the previous slide, current slide and next slide
     const previousSlideNumber = modulo(slideNumber - 1, numberOfSlides);
     const nextSlideNumber = modulo(slideNumber + 1, numberOfSlides);
-    const previousSlide =
-      slidesContainer.children[previousSlideNumber];
+    const previousSlide = slidesContainer.children[previousSlideNumber];
     const currentSlideElement = slidesContainer.children[slideNumber];
     const nextSlide = slidesContainer.children[nextSlideNumber];
 
-    // here, we're hiding the previous and next slides and showing the current slide
     previousSlide.setAttribute('aria-hidden', true);
     nextSlide.setAttribute('aria-hidden', true);
     currentSlideElement.setAttribute('aria-hidden', false);
   }
 
-  function modulo(number, mod) {
-    let result = number % mod;
-    if (result < 0) result += mod;
-    return result;
-  }
+  // get elements
+  const buttonPrevious = carousel.querySelector(
+    '[data-carousel-button-previous]'
+  );
+  const buttonNext = carousel.querySelector(
+    '[data-carousel-button-next]'
+  );
+  const slidesContainer = carousel.querySelector(
+    '[data-carousel-slides-container]'
+  );
 
-  buttonNext.addEventListener('click', handleNextSlide);
+  const numberOfSlides = slidesContainer.childElementCount;
+  let currentSlide = 0;
+
+  // set up events
   buttonPrevious.addEventListener('click', handlePreviousSlide);
+  buttonNext.addEventListener('click', handleNextSlide);
 }
+
+const carousels = document.querySelectorAll('[data-carousel]');
+carousels.forEach(setUpCarousel);
