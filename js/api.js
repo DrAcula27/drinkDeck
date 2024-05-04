0316fcb50bf6d2b35b6a8c773f58f4db9db184cf
Danielle <danielle@kendallspace.net>
2024-05-03 20:15:16 -0700

    make fetch work with drinks that have spaces in the name

--- js/api.js
+++ js/api.js
@@ -2,19 +2,24 @@ document.querySelector('button').addEventListener('click', getDrink);
 
 function getDrink() {
   let drink = document.querySelector('input').value;
+  drink = drink.split(' ').join('%20');
 
   let url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`;
 
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data.drinks[0]);
-      document.querySelector('h2').innerText =
-        data.drinks[0].strDrink;
-      document.querySelector('img').src =
-        data.drinks[0].strDrinkThumb;
-      document.querySelector('p').innerText =
-        data.drinks[0].strInstructions;
+      if (data.drinks && data.drinks.length > 0) {
+        const firstDrink = data.drinks[0];
+        document.querySelector('h2').innerText = firstDrink.strDrink;
+        document.querySelector('img').src = firstDrink.strDrinkThumb;
+        document.querySelector('p').innerText =
+          firstDrink.strInstructions;
+      } else {
+        document.querySelector('h2').innerText = 'No drinks found';
+        document.querySelector('img').src = '';
+        document.querySelector('p').innerText = '';
+      }
     })
     .catch((err) => {
       console.log(`error ${err}`);
